dcc7ad64feee5b4a5ebb0b46efac6192a95e9437
Maaaachi <87088494+Maaaachi@users.noreply.github.com>
2022-02-16 00:58:47 +0900

    Laravel入門４章課題

--- web.php
+++ web.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExTestController;
 
-Route::get('/test/{room}/{id}', function ($room, $id) {
-  return "roomが" . "$room" . "で" . "idが" . "$id" . "です";
-});
-
-Route::get('/test/{greet?}', function($greet = 'Goodmorning') {
-    return $greet . '=おはようございます';
-});
+Route::get('/{msg?}', [ExTestController::class, 'index']);
